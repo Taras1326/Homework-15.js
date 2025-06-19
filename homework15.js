ebfd917f5a1b7de3aabcad55e515e578df196198
Taras1326 <166243411+Taras1326@users.noreply.github.com>
2025-06-19 16:13:20 +0300

    hw-15

--- homework15.js
+++ homework15.js
@@ -3,12 +3,30 @@
 
 // Отримати загальну суму балансу (поле balance) всіх користувачів.
 
-const calculateTotalBalance = users =>
-  users.reduce((total, { balance }) => total + balance, 0);
+                  // варіант 1
 
-console.log(calculateTotalBalance(users)); // 20916
+const users = [
+  { name: "Nazar", balance: 3200 },
+  { name: "Ivan", balance: 4100 },
+  { name: "Vadim", balance: 5700 },
+  { name: "Taras", balance: 5100 },
+  { name: "Miroslav", balance: 2816 },
+];
 
 
+const calculateTotalBalance = users => users.reduce((total, item) => total + item.balance, 0);
+console.log(calculateTotalBalance(users));  // 20916
+
+
+
+              // варіант 2
+
+
+// const calculateTotalBalancee = users => {
+//   return users.reduce((totals, user) => totals + user.balance, 0);
+// };
+
+// console.log(calculateTotalBalancee(users)); // 20916
 
 
 
@@ -16,15 +34,21 @@ console.log(calculateTotalBalance(users)); // 20916
 
 // Масив імен всіх користувачів у яких є друг із зазначеним ім'ям.
 
+const user = [
+  { name: "Sharlene Bush", friends: ["Briana Decker", "Goldie Gentry"] },
+  { name: "Elma Head", friends: ["Goldie Gentry"] },
+  { name: "Sheree Anthony", friends: ["Briana Decker", "Goldie Gentry"] },
+  { name: "Ross Vazquez", friends: ["Marilyn Mcintosh"] }
+];
 
 
-const getUsersWithFriend = (users, friendName) =>
-  users
-    .filter(({ friends }) => friends.includes(friendName))
-    .map(({ name }) => name);
+const getUsersWithFriend = (user, friendName) => user.filter((item) => item.friends.includes(friendName)).map((item)=>item.name)
+;
 
-console.log(getUsersWithFriend(users, 'Briana Decker')); // ['Sharlene Bush', 'Sheree Anthony']
-console.log(getUsersWithFriend(users, 'Goldie Gentry')); // ['Elma Head', 'Sheree Anthony']
+console.log(getUsersWithFriend(user, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(user, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
+
 
 
 
@@ -33,13 +57,26 @@ console.log(getUsersWithFriend(users, 'Goldie Gentry')); // ['Elma Head', 'Shere
 // Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends)
 
 
-const getNamesSortedByFriendsCount = users =>
-  [...users]
-    .sort((a, b) => a.friends.length - b.friends.length)
-    .map(({ name }) => name);
+const userss = [
+  { name: "Moore Hensley", friends: [] },
+  { name: "Sharlene Bush", friends: ["A"] },
+  { name: "Elma Head", friends: ["A", "B"] },
+  { name: "Carey Barr", friends: ["A", "B", "C"] },
+  { name: "Blackburn Dotson", friends: ["A", "B", "C", "D"] },
+  { name: "Sheree Anthony", friends: ["A", "B", "C", "D", "E"] },
+  { name: "Ross Vazquez", friends: ["A", "B", "C", "D", "E", "F"] }
+];
 
-console.log(getNamesSortedByFriendsCount(users));
-// ['Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez']
+
+
+// Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends)
+
+const getNamesSortedByFriendsCount = userss => {
+  return userss.sort((a, b)=> a.friends.length - b.friends.length).map((item)=>item.name)
+};
+
+console.log(getNamesSortedByFriendsCount(userss));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 
 
@@ -49,14 +86,15 @@ console.log(getNamesSortedByFriendsCount(users));
 // Отримати масив всіх умінь всіх користувачів (поле skills), 
 // при цьому не має бути повторюваних умінь і вони повинні бути відсортовані в алфавітному порядку.
 
+const userse = [
+  { name: "User1", skills: ["html", "css", "js", "react"] },
+  { name: "User2", skills: ["node", "css", "react", "js"] },
+  { name: "User3", skills: ["js", "typescript", "node", "css"] }
+];
 
-const getSortedUniqueSkills = users => {
-    const allSkills = users.flatMap(({ skills }) => skills);
-    const uniqueSkills = [...new Set(allSkills)];
-    return uniqueSkills.sort();
-  };
+const getSortedUniqueSkills = users4 => users4.flatMap(users4 => users4.skills).filter((skill, i, arr) => arr.indexOf(skill) === i).sort();
   
-  console.log(getSortedUniqueSkills(users));
+  console.log(getSortedUniqueSkills(userse));
   // ['adipisicing', 'amet', 'anim', ..., 'veniam']
   
 
